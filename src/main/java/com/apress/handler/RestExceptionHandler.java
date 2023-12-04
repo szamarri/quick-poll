d51e43a5b31f01779e8a8a6cc96860838d8aeb58
Saul Zamarripa Rivera <saul.zamarripa.rivera@gmail.com>
2023-12-04 14:52:40 -0600

    chore: Error Detail& RestExceptionHandler

---
+++ src/main/java/com/apress/handler/RestExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.apress.handler;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.apress.error.ErrorDetail;
+import com.apress.exception.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?>  handlerResourceNotFoundException(ResourceNotFoundException rnfe, HttpServletRequest request) {
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setTimeStamp(new Date().getTime());
+        errorDetail.setStatus(HttpStatus.NOT_FOUND.value());
+        errorDetail.setTitle("Resource Not Found");
+        errorDetail.setDetail(rnfe.getMessage());
+        errorDetail.setDeveloperMessage(rnfe.getClass().getName());
+
+        return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
+    }
+}
