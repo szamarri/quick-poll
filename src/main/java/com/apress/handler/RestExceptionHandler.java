898c9d464764958a0cad5cff9e620dc6d5f6996e
Saul Zamarripa Rivera <saul.zamarripa.rivera@gmail.com>
2023-12-22 22:37:34 -0600

    chore: Improving Exception Handler

--- src/main/java/com/apress/handler/RestExceptionHandler.java
+++ src/main/java/com/apress/handler/RestExceptionHandler.java
@@ -2,13 +2,25 @@ package com.apress.handler;
 
 import java.util.Date;
 import java.util.List;
+
+import javax.inject.Inject;
+
 import java.util.ArrayList;
 
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+//import org.springframework.lang.Nullable;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springframework.validation.FieldError;
 
 import com.apress.dto.error.ErrorDetail;
@@ -18,7 +30,10 @@ import com.apress.exception.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Inject
+    private MessageSource messageSource;
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?>  handlerResourceNotFoundException(ResourceNotFoundException rnfe, HttpServletRequest request) {
@@ -32,8 +47,9 @@ public class RestExceptionHandler {
         return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleValidationError(MethodArgumentNotValidException manve, HttpServletRequest request) {
+    /*@ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public @ResponseBody ErrorDetail handleValidationError(MethodArgumentNotValidException manve, HttpServletRequest request) {
         ErrorDetail errorDetail = new ErrorDetail();
         // populate error Detail instance
         errorDetail.setTimeStamp(new Date().getTime());
@@ -56,10 +72,54 @@ public class RestExceptionHandler {
             }
             ValidationError validationError = new ValidationError();
             validationError.setCode(fe.getCode());
-            validationError.setMessage(fe.getDefaultMessage());
+            validationError.setMessage(messageSource.getMessage(fe, null));
             validationErrorList.add(validationError);
         }
 
-        return new ResponseEntity<>(errorDetail, null, HttpStatus.BAD_REQUEST);
-    }
+        return errorDetail;
+    }*/
+
+    @Override
+	public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException manve, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		
+		ErrorDetail errorDetail = new ErrorDetail();
+		// Populate errorDetail instance
+		errorDetail.setTimeStamp(new Date().getTime());
+		errorDetail.setStatus(HttpStatus.BAD_REQUEST.value());
+		errorDetail.setTitle("Validation Failed");
+		errorDetail.setDetail("Input validation failed");
+		errorDetail.setDeveloperMessage(manve.getClass().getName());
+		
+		// Create ValidationError instances
+		List<FieldError> fieldErrors =  manve.getBindingResult().getFieldErrors();
+		for(FieldError fe : fieldErrors) {
+			
+			List<ValidationError> validationErrorList = errorDetail.getErrors().get(fe.getField());
+			if(validationErrorList == null) {
+				validationErrorList = new ArrayList<ValidationError>();
+				errorDetail.getErrors().put(fe.getField(), validationErrorList);
+			}
+			ValidationError validationError = new ValidationError();
+			validationError.setCode(fe.getCode());
+			validationError.setMessage(messageSource.getMessage(fe, null));
+			validationErrorList.add(validationError);
+		}
+		
+		return handleExceptionInternal(manve, errorDetail, headers, status, request);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(
+			HttpMessageNotReadableException ex, HttpHeaders headers,
+			HttpStatusCode status, WebRequest request) {
+		
+		ErrorDetail errorDetail = new ErrorDetail();
+		errorDetail.setTimeStamp(new Date().getTime());
+		errorDetail.setStatus(status.value());
+		errorDetail.setTitle("Message Not Readable");
+		errorDetail.setDetail(ex.getMessage());
+		errorDetail.setDeveloperMessage(ex.getClass().getName());
+		
+		return handleExceptionInternal(ex, errorDetail, headers, status, request);
+	}
 }
